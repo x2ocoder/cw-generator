5d000599f472651b3eff88e9fbba3f5daa2908a4
Zakaria Lounes <zakaria.lounes@gmail.com>
2021-07-29 12:59:50 +0200

    VueJS starter pack

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp, h } from 'vue/dist/vue.cjs.prod.js'
+
+import Home from './components/Home'
+const NotFoundComponent = { template: '<p>Not found</p>'}
+
+const routes = {
+    '/': Home,
+}
+const SimpleRouter = {
+    data: () => ({
+        currentRoute: window.location.pathname
+    }),
+
+    computed: {
+        CurrentComponent() {
+            return routes[this.currentRoute] || NotFoundComponent
+        }
+    },
+
+    render() {
+        return h(this.CurrentComponent)
+    }
+}
+
+createApp(SimpleRouter).mount('#app')
