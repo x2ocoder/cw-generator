b619d7a67bbaa4dd70b7ca5aa21dee4fc1c30d9f
Zakaria Lounes <zakaria.lounes@gmail.com>
2021-08-10 15:04:11 +0200

    Switch from CRLF to LF

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,16 @@
 
     <!-- JUMBOTRON -->
     <div class="mb-0 juno-gradient text-white">
-            <div class="container p-5">
-                <h1 class="display-5 fw-bold">Create your CW20 Token</h1>
+        <div class="container p-5">
+            <h1 class="display-5 fw-bold">Create your CW20 Token</h1>
 
-                <p class="lead">
-                    Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable CW20 Token.
-                    <br>
-                    CW20 Generator is the easiest and fastest way to create your own CW20 token on the Juno network. No coding skills are required.
-                </p>
-            </div>
+            <p class="lead">
+                Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable CW20 Token.
+                <br>
+                CW20 Generator is the easiest and fastest way to create your own CW20 token on the Juno network. No coding skills are required.
+            </p>
         </div>
+    </div>
 
     <!-- FORM -->
     <ContractForm v-model:address="address"/>
